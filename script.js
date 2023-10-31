3b853be641bec02af0ab221a26521e2d7d765f21
Loïc <e282266@edu.scbl.ch>
2023-10-31 11:25:18 +0100

    Aufgabe 2 - 3

--- script.js
+++ script.js
@@ -20,11 +20,24 @@ export function aufgabe02(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-  if (currentElement ) {
-    
-  } else {
-    result.push(currentElement)
-  }
+    result.push(currentElement.toUpperCase())
   }
   return result.join("")
+}
+
+export function aufgabe03(args) {
+  const input = args
+  const result = []
+
+  const inputArray = input.split('');
+  const eCount = inputArray.filter(char => char === 'e' || char === 'E').length;
+
+  if (eCount >= 4) {
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+      result.push(currentElement)
+    }
+  }
+    return result.join("")
+
 }
