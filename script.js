7b5354e2df48134c6e86019ff92b6c93cecd1583
Loïc <e282266@edu.scbl.ch>
2023-10-31 10:02:35 +0100

    AUfgabe 1

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+
+    if (currentElement === "e" || currentElement === "E") {
+
+    } else {
+      result.push(currentElement)
+    }
+  }
+  return result.join("")
+}
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+  if (currentElement ) {
+    
+  } else {
     result.push(currentElement)
+  }
   }
   return result.join("")
 }
